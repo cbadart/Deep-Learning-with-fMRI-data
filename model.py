25c28c6a74dfd0680152e783242a1438fe066736
Catherine Badart <cbadart@DESKTOP-0V8P6TL.localdomain>
2018-04-05 17:41:03 -0400

    added code to read in training data

--- model.py
+++ model.py
@@ -2,6 +2,7 @@ from keras.models import Sequential
 from keras.layers import Dense
 from scipy.io import loadmat
 import numpy as np
+import os, glob
 
 # initialize model
 model = Sequential()
@@ -10,15 +11,9 @@ model = Sequential()
 model.add(Dense(units=64, activation='relu', input_dim=100))
 model.add(Dense(units=10, activation='softmax'))
 
-# set weights
-# weights are Numpy arrays
-# somehow set weights from labels.mat
-
 # read matlab file
 mat_labels = loadmat('labels.mat', squeeze_me=True, struct_as_record=False)
 labels = mat_labels['labels']
-
-model.set_weights(labels)
 
 # configure learning process
 model.compile(loss='categorical_crossentropy',
@@ -27,11 +22,20 @@ model.compile(loss='categorical_crossentropy',
 
 # print out summary of model
 model.summary()
-			  
+
+traing_data = []
+
 # train with training data
 # training data should be Numpy arrays
+#for root, dirnames, filenames in os.walk('fMRI'):
+#	for file in filenames:
+#		train = laodmat(file)
+#		traing_data.append(train['data'])
+
+train1subj = loadmat('fMRI/subj001.mat')['data']
+		
 #model.fit(x_train, y_train, epochs=5, batch_size=32)
-#model.train_on_batch(x_batch, y_batch)
+model.train_on_batch(train1subj, labels)
 
 # evaluate performance
 #loss_and_metrics = model.evaluate(x_test, y_test, batch_size=128)
