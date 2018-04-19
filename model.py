c36c6fd0ec1e5ed4ab17fe62aa63dabb0adb9178
Catherine Badart <cbadart@DESKTOP-0V8P6TL.localdomain>
2018-04-19 15:06:40 -0400

    fixed input layer shape, added hidden layer, output layer shape is still wrong

--- model.py
+++ model.py
@@ -8,10 +8,14 @@ import os, glob
 model = Sequential()
 
 # add layers
-model.add(Dense(units=64, activation='relu', input_dim=100))
-model.add(Dense(units=10, activation='softmax'))
+# input layer: data is 1940 x 116 so input layer should have 116 nodes
+model.add(Dense(units=116, activation='relu', input_dim=116))
+# hidden layer: ~2/3(n_in) + n_out = 84
+model.add(Dense(units=84, activation='relu'))
+# output layer: one node per class label when using softmax, 7 classes so 7 nodes
+model.add(Dense(units=7, activation='softmax'))
 
-# read matlab file
+# read matlab label file
 mat_labels = loadmat('labels.mat', squeeze_me=True, struct_as_record=False)
 labels = mat_labels['labels']
 
@@ -33,6 +37,7 @@ traing_data = []
 #		traing_data.append(train['data'])
 
 train1subj = loadmat('fMRI/subj001.mat')['data']
+print(train1subj.shape)
 		
 #model.fit(x_train, y_train, epochs=5, batch_size=32)
 model.train_on_batch(train1subj, labels)
