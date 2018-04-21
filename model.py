ec738c8bf53d04eca882872f1a46fdae819a3d8a
Catherine Badart <cbadart@DESKTOP-0V8P6TL.localdomain>
2018-04-20 22:10:34 -0400

    finished ten fold xv

--- model.py
+++ model.py
@@ -2,7 +2,6 @@ import keras
 from keras.models import Sequential
 from keras.layers import Dense, Flatten
 from scipy.io import loadmat
-import keras
 import numpy as np
 import os, glob
 import pprint
@@ -21,10 +20,11 @@ def load_data():
 	print(data.shape)
 	return data
 
-def ten_fold_xv(data):
+def ten_fold_xv(model, labels, data):
 	# does 10-fold cross validation
-	results = dict()
+	results = []
 	for i in range(10):
+		print("Testing Set {}".format(i))
 		test = np.zeros(shape=(10, 1940, 116))
 		train = np.zeros(shape=(90, 1940, 116))
 		# partition data into test and train
@@ -40,17 +40,29 @@ def ten_fold_xv(data):
 				train_i = train_i + 1
 		
 		# train model
-		
-		# evaluate performance
-		#loss_and_metrics = model.evaluate(x_test, y_test, batch_size=128)
-		#results[i] = loss_and_metrics["accuracy"] #??? maybe ???
+		subj_num = 0
+		print("\ttraining...")
+		for subj in train:
+			model.fit(subj, keras.utils.to_categorical(labels), verbose=0)
+			#print("Training subject {}".format(subj_num))
+			subj_num = subj_num + 1
+	
+		# evaluate performance with test data
+		accuracy = 0
+		print("\ttesting...")
+		for subj in test:
+			loss_and_metrics = model.evaluate(subj, keras.utils.to_categorical(labels), verbose=0)
+			accuracy = accuracy + loss_and_metrics[1]
+		results.append(accuracy/10.0)
 		
 	# calculate overall accuracy
-	print("----- RESULTS -----")
+	print("\n----- RESULTS -----")
 	total = 0
+	ind = 0
 	for instance in results:
-		total = total + results[instance]
-		print("{}: {}".format(instance, results[instance]))
+		total = total + instance
+		print("{}: {}".format(ind, instance))
+		ind = ind + 1
 	avg = total / 10.0
 	print("\nOverall: {}".format(avg))
 	
@@ -60,9 +72,11 @@ def main():
 
 	# add layers
 	# input layer: data is 1940 x 116 so input layer should have 116 nodes
-
+	# change to 90 units once we clean up data since only 90 of the 116 regions are used
 	model.add(Dense(units=116, activation='relu', input_dim=116))
+	
 	# hidden layer: ~2/3(n_in) + n_out = 84
+	# will be 67 if input only has 90
 	model.add(Dense(units=84, activation='relu'))
 
 	# output layer: one node per class label when using softmax, 7 classes so 7 nodes
@@ -81,34 +95,32 @@ def main():
 	model.summary()
 	
 	# load fMRI data from the matlab files
-
 	data = load_data()
-	pprint.pformat(data)
-	train = data[0:89]
-	print("train shape: {}".format(train.shape))
-	test = data[90:99]
-	print("test shape: {}".format(test.shape))
+	#pprint.pformat(data)
+	
+	ten_fold_xv(model, labels, data)
+	# train = data[0:89]
+	# print("train shape: {}".format(train.shape))
+	# test = data[90:99]
+	# print("test shape: {}".format(test.shape))
 			
-	#model.fit(x_train, y_train, epochs=5, batch_size=32)
-	subj_num = 0
+	
+	# subj_num = 0
+	# for subj in train:
+		# model.fit(subj, keras.utils.to_categorical(labels), verbose=0)
+		# print("Training subject {}".format(subj_num))
+		# subj_num = subj_num + 1
 
-	for subj in train:
-		model.fit(subj, keras.utils.to_categorical(labels))
-		print("Training subject {}".format(subj_num))
-		subj_num = subj_num + 1
+	# # evaluate performance
+	# results = []
+	# for test_subj in test:
+		# results.append(model.evaluate(test_subj, keras.utils.to_categorical(labels)))#, batch_size=128))
 
-	#ten_fold_xv(data)
-	# evaluate performance
-	results = []
-	for test_subj in test:
-		results.append(model.evaluate(test_subj, keras.utils.to_categorical(labels)))#, batch_size=128))
+	# print(results)
+	# #pprint.pprint(results)
 
-	pprint.pprint(results)
-
-	return
-
-	# generate predictions on new data
-	#classes = model.predict(x_test, batch_size=128)
+	# # generate predictions on new data
+	# #classes = model.predict(x_test, batch_size=128)
 
 if __name__ == '__main__':
 	main()
